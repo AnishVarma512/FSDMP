4ff8803d1be64e4247fecc2963e7d445dd364a53
AnishVarma512 <115741903+AnishVarma512@users.noreply.github.com>
2022-11-22 13:38:10 +0530

    Add files via upload

---
+++ portfolio-master/assets/js/script.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+let fileInput = document.getElementById("file-input");
+let fileList = document.getElementById("files-list");
+let numOfFiles = document.getElementById("num-of-files");
+
+fileInput.addEventListener("change", () => {
+  fileList.innerHTML = "";
+  numOfFiles.textContent = `${fileInput.files.length} Files Selected`;
+
+  for (i of fileInput.files) {
+    let reader = new FileReader();
+    let listItem = document.createElement("li");
+    let fileName = i.name;
+    let fileSize = (i.size / 1024).toFixed(1);
+    listItem.innerHTML = `<p>${fileName}</p><p>${fileSize}KB</p>`;
+    if (fileSize >= 1024) {
+      fileSize = (fileSize / 1024).toFixed(1);
+      listItem.innerHTML = `<p>${fileName}</p><p>${fileSize}MB</p>`;
+    }
+    fileList.appendChild(listItem);
+  }
+});
